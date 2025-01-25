a9f79981a6d48fb0f25fe49f30031c5f73d8c9c7
arielguerron14 <caguerronp@uce.edu.ec>
2025-01-24 19:45:02 -0500

    feat: :sparkles: configuration of each of the files in the support-ticket-service folder

--- services/8/src/utils/validation.js
+++
@@ -1,6 +0,0 @@
-const validateVendor = (vendor) => {
-  const { name, service, contactInfo } = vendor;
-  return name && service && contactInfo;
-};
-
-module.exports = { validateVendor };
--- services/support-ticket-service/index.js
+++ services/support-ticket-service/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const vendorRoutes = require('./src/routes/vendorRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -15,10 +14,11 @@ mongoose.connect(process.env.MONGO_URI, {
   console.error('Database connection error:', err);
 });
 
-app.use('/api/vendors', vendorRoutes);
+app.get('/health', (req, res) => {
+  res.send('Support Ticket Service is running');
+});
 
-const PORT = process.env.PORT || 3017;
-
+const PORT = process.env.PORT || 3013;
 app.listen(PORT, () => {
-  console.log(`Vendor Management Service running on port ${PORT}`);
+  console.log(`Support Ticket Service running on port ${PORT}`);
 });
