b74976f613493d6211c4436343441b10da177a4c
arielguerron14 <caguerronp@uce.edu.ec>
2025-01-25 14:00:51 -0500

    feat: :sparkles: Update the task folder in the backend

--- backend/shared-libraries/logger/logger.js
+++ backend/shared-libraries/logger/logger.js
@@ -2,15 +2,10 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
-    })
-  ),
+  format: winston.format.json(),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/app.log' }),
+    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
